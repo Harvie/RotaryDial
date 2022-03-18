866ac3733474a2a7377c3ac3c308c3407ecaa39c
Jaap Klevering <jaap.klevering@gmail.com>
2022-03-18 13:41:54 +0200

    Update dial_read.ino
    I wanted to catch the final number in a variable finalNumber, so that this can be sent to for example the tx port. So I added two lines in the setup: String num = "";  and String finalNumber = ""; And made a change in the beginning of the loop.

--- examples/dial_read/dial_read.ino
+++ examples/dial_read/dial_read.ino
@@ -4,22 +4,35 @@
 #define ROTARY_DIAL_TIMEOUT 3000 //If no numbers are dialed for this period, newline is sent
 
 void setup() {
-  Serial.begin(9600);
+  SerialUSB.begin(9600);
   RotaryDial::setup(ROTARY_DIAL_PULSE_PIN);
 }
 
 unsigned long lastdigit = 0;
 int newline = true;
 
-void loop() {
+String num = "";
+String finalNumber = "";
+
+void loop() {   
+
   if(RotaryDial::available()) {
-    Serial.print(RotaryDial::read());
+    num = String (RotaryDial::read());
+    finalNumber = "" + num;
+  //  SerialUSB.print(RotaryDial::read());
+    
+    SerialUSB.print(String(finalNumber));
+ 
     lastdigit = millis();
     newline = false;
+    
+  
   } else {
     if(!newline && millis() - lastdigit > ROTARY_DIAL_TIMEOUT) {
-      Serial.println(" OK");
+      SerialUSB.println(" OK");
+finalNumber="";
       newline = true;
     }
   }
-}
+
+  }
